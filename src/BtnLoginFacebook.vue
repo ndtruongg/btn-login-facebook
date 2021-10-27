6ba2a892e4b5df44b0596c5e37003df92e7658c4
truongnd-f1-d2 <truongnd2@fabbi.io>
2021-10-27 13:46:52 +0700

    update 1.1

---
+++ src/BtnLoginFacebook.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <button :class="btnClass" @click="logInWithFacebook">
+      <slot></slot>
+    </button>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'BtnLoginFacebook',
+  props: {
+    btnClass: {
+      type: String
+    }
+  },
+  async created() {
+    await this.initFacebook()
+    await this.loadFacebookSDK(document, 'script', 'facebook-jssdk')
+  },
+  methods: {
+    async initFacebook() {
+      window.fbAsyncInit = function () {
+        window.FB.init({
+          appId: process.env.VUE_APP_FACEBOOK_APP_ID, //You will need to change this
+          cookie: true, // This is important, it's not enabled by default
+          xfbml: true,
+          version: 'v13.0'
+        })
+        window.FB.AppEvents.logPageView()
+      }
+    },
+
+    async logInWithFacebook() {
+      const _vue = this
+
+      window.FB.login(async function (response) {
+        if (response.authResponse) {
+          _vue.getProfile(response.authResponse)
+        } else {
+          alert('User cancelled login or did not fully authorize.')
+        }
+      })
+      return false
+    },
+
+    async loadFacebookSDK(d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0]
+      if (d.getElementById(id)) {
+        return
+      }
+      js = d.createElement(s)
+      js.id = id
+      js.src = 'https://connect.facebook.net/en_US/sdk.js'
+      fjs.parentNode.insertBefore(js, fjs)
+    },
+
+    async getProfile(data) {
+      await axios
+        .get(
+          `https://graph.facebook.com/${data.userID}?fields=id,name,email,picture&access_token=${data.accessToken}`
+        )
+        .then((response) => {
+          this.$emit('profile', response.data)
+        })
+        .catch()
+    }
+  }
+}
+</script>
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import BtnLoginFacebook from './BtnLoginFacebook.vue'
+
+function install(Vue) {
+  if (install.installed) return
+  install.installed = true
+  Vue.component('BtnLoginFacebook', BtnLoginFacebook)
+}
+
+const plugin = {
+  install
+}
+
+let GlobalVue = null
+if (typeof window !== 'undefined') {
+  GlobalVue = window.Vue
+} else if (typeof global !== 'undefined') {
+  GlobalVue = global.vue
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin)
+}
+
+BtnLoginFacebook.install = install
+
+export default BtnLoginFacebook
